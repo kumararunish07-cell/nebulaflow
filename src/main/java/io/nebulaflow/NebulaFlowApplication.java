6b7d12b2dd632fc3840edfb1219e27bd8c83b9c1
kumararunish07-cell <kumararunish07@gmail.com>
2026-09-12 00:20:50 +0530

    Add src/main/java/io/nebulaflow/NebulaFlowApplication.java

---
+++ src/main/java/io/nebulaflow/NebulaFlowApplication.java
@@ -0,0 +1,9 @@
+﻿package io.nebulaflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NebulaFlowApplication {
+  public static void main(String[] args) { SpringApplication.run(NebulaFlowApplication.class, args); }
+}
