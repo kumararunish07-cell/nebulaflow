d5d504e895ad95cef876e07e978a4e90f71ea15d
kumararunish07-cell <kumararunish07@gmail.com>
2026-09-12 00:21:51 +0530

    Add src/test/java/io/nebulaflow/engine/ExecutionEngineTest.java

---
+++ src/test/java/io/nebulaflow/engine/ExecutionEngineTest.java
@@ -0,0 +1,3 @@
+﻿package io.nebulaflow.engine;
+import com.fasterxml.jackson.databind.ObjectMapper; import org.junit.jupiter.api.Test; import java.util.*; import static org.junit.jupiter.api.Assertions.*;
+class ExecutionEngineTest { @Test void transformsValues(){var e=new ExecutionEngine(new ObjectMapper());var d=Map.<String,Object>of("steps",List.of(Map.of("id","start","type","TRANSFORM","config",Map.of("value","hello","expression","uppercase"))));var out=e.execute(d,Map.of());assertEquals("HELLO",((Map<?,?>)out.get("steps")).get("start"));} }
